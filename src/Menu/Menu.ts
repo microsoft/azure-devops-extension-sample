4c52159703cb7c14170f54a930f024970ed0ffc9
Nick Kirchem <nickkirc@microsoft.com>
2018-10-03 14:52:55 -0400

    Call a build api in the menu extension sample

--- src/Menu/Menu.ts
+++ src/Menu/Menu.ts
@@ -1,11 +1,18 @@
 import "es6-promise/auto";
 import * as DevOps from "azure-devops-extension-sdk";
 
+import { getClient } from "azure-devops-extension-api/extension";
+import { BuildRestClient } from "azure-devops-extension-api/clients/Build";
+import { BuildDefinition } from "azure-devops-extension-api/types/Build";
+
 DevOps.register("sample-build-menu", () => {
     return {
-        execute: (context: any) => {
-            DevOps.getService<DevOps.IHostDialogService>(DevOps.CommonServiceIds.HostDialogService).then((dialogSvc) => {
-                dialogSvc.openMessageDialog("Custom message from extension. Context: " + JSON.stringify(context), { showCancel: false });
+        execute: (context: BuildDefinition) => {
+
+            getClient(BuildRestClient).getDefinition(context.id, context.project.id, undefined, undefined, undefined, true).then(result => {
+                DevOps.getService<DevOps.IHostDialogService>(DevOps.CommonServiceIds.HostDialogService).then((dialogSvc) => {
+                    dialogSvc.openMessageDialog(`Fetched build definition ${result.name}. Latest build: ${JSON.stringify(result.latestBuild)}`, { showCancel: false });
+                });
             });
         }
     }
