f296198bc60f1f54258c168e86cfd296ab11d5b2
Will Smythe <wismythe@microsoft.com>
2018-10-23 09:33:58 -0400

    Update azure-pipeline.yml
    Bump node js version
    
    Fix prep command on Linux

---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const fs = require("fs");
+
+// Webpack entry points. Mapping from resulting bundle name to the source file entry.
+const entries = {};
+
+// Loop through subfolders in the "Pages" folder and add an entry for each one
+const pagesDir = path.join(__dirname, "src/Pages");
+fs.readdirSync(pagesDir).filter(dir => {
+    if (fs.statSync(path.join(pagesDir, dir)).isDirectory()) {
+        entries[dir] = path.resolve(path.relative(process.cwd(), path.join(pagesDir, dir, dir)));
+    }
+});
+
+module.exports = {
+    entry: entries,
+    output: {
+        filename: path.relative(process.cwd(), path.join(__dirname, "[name]", "[name].js"))
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "vss-ui/buildScripts/css-variables-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.woff$/,
+                use: [{
+                    loader: 'base64-inline-loader'
+                }]
+            },
+            {
+                test: /\.html$/,
+                loader: "file-loader"
+            }
+        ]
+    }
+};
