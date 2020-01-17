7bfbd4147802964a84c309758f55db9892d9391f
Nick Kirchem <nickkirc@microsoft.com>
2020-01-17 18:36:01 -0500

    Consume the latest azure-devops-ui package, adding the required global css overrides import.

--- src/Common.tsx
+++ src/Common.tsx
@@ -1,8 +1,8 @@
+import "azure-devops-ui/Core/override.css";
 import "es6-promise/auto";
-import "./Common.scss";
-
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "./Common.scss";
 
 export function showRootComponent(component: React.ReactElement<any>) {
     ReactDOM.render(component, document.getElementById("root"));
