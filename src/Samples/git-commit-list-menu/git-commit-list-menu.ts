2b9c131f42c9bc7545b676199b83a509657b5330
Dan Hellem <dan_hellem@hotmail.com>
2024-10-11 11:42:35 -0400

    Fix to git-commit-list-menu

--- src/Samples/git-commit-list-menu/git-commit-list-menu.ts
+++ src/Samples/git-commit-list-menu/git-commit-list-menu.ts
@@ -1,7 +1,7 @@
 import "es6-promise/auto";
 import * as SDK from "azure-devops-extension-sdk";
 
-SDK.register("git-branches-tree-menu", () => {
+SDK.register("git-commit-list-menu", () => {
     return {
         execute: async (context: any) => {
             alert("Custom Action!");
